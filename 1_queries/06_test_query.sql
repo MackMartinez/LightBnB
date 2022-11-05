c1efe919b1808e8bfb002a9751af5d091b138d10
MackyClap <martimac2320@gmail.com>
2022-11-05 17:17:02 -0400

    addproperty updated to use promises

--- 1_queries/06_test_query.sql
+++ 1_queries/06_test_query.sql
@@ -1,7 +1,2 @@
-SELECT properties.id, properties.title, properties.cost_per_night, AVG(property_reviews.rating) AS average_rating
-  FROM properties
-  LEFT JOIN property_reviews ON property_id = properties.id
-  GROUP BY properties.id
-  HAVING AVG(property_reviews.rating) >= 3
-  ORDER BY cost_per_night
-  LIMIT 10;
+SELECT id, title FROM properties
+ORDER BY id DESC;
